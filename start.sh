18e5bb3976023aaf0ed65060724122fd9c481ac2
aaalalpe <aaalalpe@jyu.fi>
2024-12-03 13:20:46 +0200

    Fix supervisors being separate from orchestrator in compose file

--- start.sh
+++ start.sh
@@ -2,13 +2,7 @@
 
 DIR=$(dirname "${BASH_SOURCE[0]}")
 export $(cat "${DIR}/.env" | xargs)
-
-# Start the supervisors first in detached mode
-docker compose -f "${DIR}/compose.yml" --profile device up -d --pull always
-# docker compose -f "compose.yml" --profile device up --pull always
-
-# Start the orchestrator last
-docker compose -f "${DIR}/wasmiot-orchestrator/docker-compose.yml" up --build
+docker compose -f "${DIR}/compose.yml" --profile device up --pull always
 
 
 
